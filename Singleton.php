9a3584c97975b2c802c163f6e9bad76b3af1d473
stealth35 <jinmoku@hotmail.com>
2011-02-23 21:13:41 +0100

    add file

---
+++ Singleton.php
@@ -0,0 +1,30 @@
+<?php
+class Singleton
+{
+    protected static $_instances;
+   
+    public static function __callStatic($name, array $arguments)
+    {    
+        if(empty(static::$_instances[$name]) || !empty($arguments))
+        {
+                if(strpos($name, '\\') === 0)
+                {
+                $name = substr($name, 1);
+                }
+               
+                $classname = '\\' . $name;
+
+            if(method_exists($classname, '__construct'))
+            {
+                $class = new \ReflectionClass($classname);
+                static::$_instances[$name] = $class->newInstanceArgs($arguments);
+            }
+            else
+            {
+                static::$_instances[$name] = new $classname;
+            }
+        }
+
+        return static::$_instances[$name];
+    }
+}
